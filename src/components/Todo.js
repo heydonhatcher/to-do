cb7290198488ec752b8db9f860a0ab3cf48de6d4
heydonhatcher <heyhatch@gmail.com>
2020-01-22 15:55:40 -0600

    to do list working

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Todo = () => {
-  return (
-    <div>
-      <input type="text" />
-      <button>Add Task</button>
-    </div>
-  );
-};
+class Todo extends Component {
+  state = {
+    text: "",
+    listItems: []
+  };
+
+  handleTextChange = e => {
+    this.setState({ text: e.target.value });
+  };
+
+  handleClicked = () => {
+    const items = [...this.state.listItems];
+    items.push(this.state.text);
+    this.setState({ listItems: items });
+  };
+
+  render() {
+    return (
+      <div>
+        <input type="text" onChange={this.handleTextChange} />
+        <button onClick={this.handleClicked}>Add Task</button>
+        {this.state.listItems.map(value => {
+          return <p>{value}</p>;
+        })}
+      </div>
+    );
+  }
+}
 
 export default Todo;
