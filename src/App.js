c202ce0cb6254602ba427740735c0d4a219262a6
heydonhatcher <heyhatch@gmail.com>
2020-01-21 20:14:21 -0600

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div className="App">
+      <Todo name="test name" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Todo = () => {
+  return (
+    <div>
+      <input type="text" />
+      <button>Add Task</button>
+    </div>
+  );
+};
+
+export default Todo;
